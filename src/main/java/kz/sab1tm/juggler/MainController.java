abd5592fde1331e5134709b3834e66ed63206504
Sabit Murzaliev <sabit.murzaliev@gmail.com>
2024-09-15 17:41:01 +0500

    add switch themes

--- src/main/java/kz/sab1tm/juggler/MainController.java
+++ src/main/java/kz/sab1tm/juggler/MainController.java
@@ -1,15 +1,41 @@
 package kz.sab1tm.juggler;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.Parent;
+import javafx.scene.control.ComboBox;
+
+import java.util.Objects;
 
 public class MainController {
 
     @FXML
-    private Label welcomeText;
+    private Parent root;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ComboBox<String> leftActionSelectTheme;
+
+    @FXML
+    private void initialize() {
+        leftActionSelectTheme.setValue("Dark"); // Установите тему по умолчанию
+    }
+
+    public void switchToDarkTheme() {
+        root.getStylesheets().clear();
+        root.getStylesheets().add(Objects.requireNonNull(getClass().getResource("css/dark.css")).toExternalForm());
+    }
+
+    public void switchToLightTheme() {
+        root.getStylesheets().clear();
+        root.getStylesheets().add(Objects.requireNonNull(getClass().getResource("css/light.css")).toExternalForm());
+    }
+
+    @FXML
+    private void onThemeChange() {
+        String selectedTheme = leftActionSelectTheme.getValue();
+        if ("Dark".equals(selectedTheme)) {
+            switchToDarkTheme();
+        } else if ("Light".equals(selectedTheme)) {
+            switchToLightTheme();
+        }
     }
 }
